f40ed31d4d4806d1f67265c9c34250b39a0310a8
XQuang1607 <quangnxde160580@fpt.edu.vn>
2024-12-15 08:59:23 +0700

    update format code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import './App.scss';
+import "./App.scss";
 // react router dom
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages
 import { Home, MealDetails, Error, Category } from "./pages/index";
 // components
@@ -12,30 +8,18 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-    return ( <
-        BrowserRouter >
-        <
-        Header / >
-        <
-        Sidebar / >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Home / > }
-        /> <
-        Route path = "/meal/:id"
-        element = { < MealDetails / > }
-        /> <
-        Route path = "/meal/category/:name"
-        element = { < Category / > }
-        /> <
-        Route path = "*"
-        element = { < Error / > }
-        /> <
-        /Routes> <
-        /BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="/meal/:id" element={<MealDetails />} />{" "}
+        <Route path="/meal/category/:name" element={<Category />} />{" "}
+        <Route path="*" element={<Error />} />{" "}
+      </Routes>{" "}
+    </BrowserRouter>
+  );
 }
 
 export default App;
